20d95a64acd8d0a1b10973b23ba35532d7926fd0
LB-king <731540792@qq.com>
2019-05-22 21:28:00 +0800

    add sth

--- VueJs/vue-demo/vue.config.js
+++ VueJs/vue-demo/vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    devServer:{
-        open:true, //编译完成后自动打开浏览器
-        port:8080,//端口号
+    devServer: {
+        open: true, //编译完成后自动打开浏览器
+        port: 8080, //端口号
     },
     //打包后的代码点击index.html文件可以直接访问项目
-    publicPath:process.env.NODE_ENV === 'production'?'./':'/',
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
 }
